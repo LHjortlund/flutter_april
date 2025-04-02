7a488a30e28100ecf584e1f5c1f59d6a14525fb5
LHjortlund <lbfe44184@edu.ucl.dk>
2025-04-02 15:27:19 +0200

    pages created

--- lib/views/widget_tree.dart
+++ lib/views/widget_tree.dart
@@ -1,5 +1,19 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_april/widgets/navbar_widget.dart';
+import 'package:flutter_april/views/pages/home_page.dart';
+import 'package:flutter_april/views/pages/profile_page.dart';
+import 'package:flutter_april/views/pages/search_page.dart';
+import 'package:flutter_april/views/pages/settings_page.dart';
+// This is a widget tree that will be used in the main.dart file
+// It will be used to create the main structure of the app
+
+List<Widget> pages = [
+  const HomePage(),
+  const ProfilePage(),
+  const SearchPage(),
+  const SettingsPage(),
+];
+// This is a list of pages that will be used in the bottom navigation bar
 
 class WidgetTree extends StatelessWidget {
   const WidgetTree({super.key});
@@ -8,6 +22,7 @@ class WidgetTree extends StatelessWidget {
   Widget build(BuildContext) {
     return Scaffold(
       appBar: AppBar(title: Text('Flutter April-test')),
+      body: pages.elementAt(0),
       bottomNavigationBar: NavbarWidget(),
     );
   }
