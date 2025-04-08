8215c7d562b73af9154a7c48a9488414f7ac1549
LHjortlund <lbfe44184@edu.ucl.dk>
2025-04-08 11:52:12 +0200

    added dropdown button to searchpage

--- lib/views/pages/search_page.dart
+++ lib/views/pages/search_page.dart
@@ -1,10 +1,48 @@
 import 'package:flutter/material.dart';
 
-class SearchPage extends StatelessWidget {
+class SearchPage extends StatefulWidget {
   const SearchPage({super.key});
 
   @override
+  State<SearchPage> createState() => _SearchPageState();
+}
+
+class _SearchPageState extends State<SearchPage> {
+  TextEditingController controller = TextEditingController();
+  String? menuItem = 'e1';
+
+  @override
   Widget build(BuildContext context) {
-    return Center(child: Text('Search Page'));
+    return SingleChildScrollView(
+      child: Padding(
+        padding: const EdgeInsets.all(20.0),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            DropdownButton(
+              value: menuItem,
+              items: [
+                DropdownMenuItem(value: 'e1', child: Text('Element 1')),
+                DropdownMenuItem(value: 'e2', child: Text('Element 2')),
+                DropdownMenuItem(value: 'e3', child: Text('Element 3')),
+              ],
+              onChanged: (String? value) {
+                setState(() {
+                  menuItem = value;
+                });
+              },
+            ),
+            TextField(
+              controller: controller,
+              decoration: InputDecoration(border: OutlineInputBorder()),
+              onEditingComplete: () {
+                setState(() {});
+              },
+            ),
+            Text(controller.text),
+          ],
+        ),
+      ),
+    );
   }
 }
