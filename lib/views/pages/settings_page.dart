c802aa69b33693a25a39f32630c6b52d443e1dad
LHjortlund <lbfe44184@edu.ucl.dk>
2025-04-08 14:51:28 +0200

    logout button activated

--- lib/views/pages/settings_page.dart
+++ lib/views/pages/settings_page.dart
@@ -1,10 +1,30 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_april/views/pages/welcome_page.dart';
 
 class SettingsPage extends StatelessWidget {
   const SettingsPage({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return Center(child: Text('Settings Page'));
+    return Padding(
+      padding: EdgeInsets.all(20.0),
+      child: Column(
+        children: [
+          ListTile(
+            title: Text('Logout'),
+            onTap: () {
+              Navigator.pushReplacement(
+                context,
+                MaterialPageRoute(
+                  builder: (context) {
+                    return const WelcomePage();
+                  },
+                ),
+              );
+            },
+          ),
+        ],
+      ),
+    );
   }
 }
