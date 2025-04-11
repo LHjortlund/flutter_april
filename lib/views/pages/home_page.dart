ff7b3e63becddb6659a79d05e7c79740ceac93dd
LHjortlund <lbfe44184@edu.ucl.dk>
2025-04-11 08:36:20 +0200

    implemented card + constants textstyle

--- lib/views/pages/home_page.dart
+++ lib/views/pages/home_page.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_april/data/constants.dart';
 import 'package:flutter_april/views/widgets/hero_widget.dart';
 
 class HomePage extends StatelessWidget {
@@ -6,9 +7,34 @@ class HomePage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Padding(
-      padding: EdgeInsets.all(20.0),
-      child: Column(children: [HeroWidget()]),
+    return SingleChildScrollView(
+      child: Padding(
+        padding: EdgeInsets.all(20.0),
+        child: Column(
+          children: [
+            HeroWidget(),
+            Container(
+              width: double.infinity,
+              padding: EdgeInsets.symmetric(vertical: 10.0),
+              child: Card(
+                child: Padding(
+                  padding: EdgeInsets.all(20.0),
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      Text('Basic Layout', style: KTextStyle.titleTealText),
+                      Text(
+                        'This is a basic layout of the app.',
+                        style: KTextStyle.descriptionText,
+                      ),
+                    ],
+                  ),
+                ),
+              ),
+            ),
+          ],
+        ),
+      ),
     );
   }
 }
