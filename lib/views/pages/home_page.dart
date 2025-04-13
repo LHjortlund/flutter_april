5efe5d0b31e608a09769ec1cb0289d83dbd3727b
LHjortlund <lbfe44184@edu.ucl.dk>
2025-04-13 19:03:12 +0200

    lotties + fittedbox + stylebutton

--- lib/views/pages/home_page.dart
+++ lib/views/pages/home_page.dart
@@ -1,7 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_april/data/constants.dart';
+import 'package:flutter_april/views/widget_tree.dart';
 import 'package:lottie/lottie.dart';
-import 'package:flutter_april/views/widgets/hero_widget.dart';
 
 class HomePage extends StatelessWidget {
   const HomePage({super.key});
@@ -14,6 +14,29 @@ class HomePage extends StatelessWidget {
         child: Column(
           children: [
             Lottie.asset('assets/lotties/animation.json'),
+            FittedBox(
+              child: Text(
+                'Welcome to the Home Page',
+                style: KTextStyle.titleTealText.copyWith(letterSpacing: 1.5),
+              ),
+            ),
+            SizedBox(height: 20.0),
+            FilledButton(
+              onPressed: () {
+                Navigator.pushReplacement(
+                  context,
+                  MaterialPageRoute(
+                    builder: (context) {
+                      return WidgetTree();
+                    },
+                  ),
+                );
+              },
+              style: FilledButton.styleFrom(
+                minimumSize: Size(double.infinity, 50),
+              ),
+              child: Text('Go to Widget Tree'),
+            ),
             Container(
               width: double.infinity,
               padding: EdgeInsets.symmetric(vertical: 10.0),
